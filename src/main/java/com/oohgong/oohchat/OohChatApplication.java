2e0e98f4fa752ac72cffe3d3aed1a8eac7cd1e12
5sbread <oohogong@gmail.com>
2023-01-09 00:43:16 +0900

    init

---
+++ src/main/java/com/oohgong/oohchat/OohChatApplication.java
@@ -0,0 +1,13 @@
+package com.oohgong.oohchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OohChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OohChatApplication.class, args);
+	}
+
+}
