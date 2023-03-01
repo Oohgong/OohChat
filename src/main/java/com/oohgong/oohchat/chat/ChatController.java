80c0f393377ca88e9404624627987389e4323436
5sbread <oohogong@gmail.com>
2023-03-01 23:11:01 +0900

    jsp / controller

--- src/main/java/com/oohgong/oohchat/chat/ChatController.java
+++ src/main/java/com/oohgong/oohchat/chat/ChatController.java
@@ -1,12 +1,14 @@
 package com.oohgong.oohchat.chat;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
